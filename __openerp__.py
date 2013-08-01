13be684f3e592eb2237b5d6bee09814b83cfe338
alorimier <anael.lorimier@xcg-consulting.fr>
2013-08-01 17:41:09 +0200

    Initial commit, add view and code to tranfer analytic codes to account_move_lines from account_invoice_lines, and from purchase_order_lines to account_invoice_lines

---
+++ __openerp__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+##############################################################################
+{
+    "name": "Account Invoice Streamline",
+    "version": "1.1",
+    "author": "XCG Consulting",
+    "category": 'Accounting',
+    "description": """Enhancements to the account
+    invoice module to streamline its usage.
+    """,
+    'website': 'http://www.openerp-experts.com',
+    'init_xml': [],
+    "depends": [
+        'base',
+        'account_streamline',
+        'analytic_structure'],
+    "data": [
+        'views/account_invoice.xml',
+    ],
+    #'demo_xml': [],
+    'test': [],
+    'installable': True,
+    'active': False,
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
---
+++ account_invoice.py
@@ -0,0 +1,123 @@
+from openerp.osv import fields, osv
+from lxml import etree
+
+
+class account_invoice_line_analytic(osv.Model):
+    _name = "account.invoice.line"
+    _inherit = "account.invoice.line"
+
+    _columns = {
+        'a1_id': fields.many2one(
+            'analytic.code',
+            "Analysis Code 1",
+            domain=[('nd_id.ns_id.model_name', '=', 'account_invoice_line')]
+        ),
+        'a2_id': fields.many2one(
+            'analytic.code',
+            "Analysis Code 2",
+            domain=[('nd_id.ns_id.model_name', '=', 'account_invoice_line')]
+        ),
+        'a3_id': fields.many2one(
+            'analytic.code',
+            "Analysis Code 3",
+            domain=[('nd_id.ns_id.model_name', '=', 'account_invoice_line')]
+        ),
+        'a4_id': fields.many2one(
+            'analytic.code',
+            "Analysis Code 4",
+            domain=[('nd_id.ns_id.model_name', '=', 'account_invoice_line')]
+        ),
+        'a5_id': fields.many2one(
+            'analytic.code',
+            "Analysis Code 5",
+            domain=[('nd_id.ns_id.model_name', '=', 'account_invoice_line')]
+        ),
+    }
+
+    def fields_view_get(self, cr, uid, view_id=None, view_type=False,
+                        context=None, toolbar=False, submenu=False):
+        print "FIELDS VIEW GET CALLED"
+        '''
+        Display analysis code in account move lines trees
+        '''
+        if context is None:
+            context = {}
+        print "Context is"
+        print context
+        res = super(account_invoice_line_analytic, self).fields_view_get(
+            cr, uid, view_id=view_id,
+            view_type=view_type, context=context,
+            toolbar=toolbar, submenu=False)
+        ans_obj = self.pool.get('analytic.structure')
+
+        #display analysis codes only when present on a related structure,
+        #with dimension name as label
+        ans_ids = ans_obj.search(cr, uid,
+                                 [('model_name', '=', 'account_invoice_line')],
+                                 context=context)
+        print "ANS IDS"
+        print ans_ids
+        ans_br = ans_obj.browse(cr, uid, ans_ids, context=context)
+        ans_dict = dict()
+        for ans in ans_br:
+            ans_dict[ans.ordering] = ans.nd_id.name
+
+        if 'fields' in res and 'invoice_line' in res['fields']:
+            print "OK"
+            doc = etree.XML(
+                res['fields']['invoice_line']['views']['tree']['arch']
+            )
+            line_fields = res['fields']['invoice_line'][
+                'views']['tree']['fields']
+
+            if 'a1_id' in line_fields:
+                line_fields['a1_id']['string'] = ans_dict.get('1', 'A1')
+                doc.xpath("//field[@name='a1_id']")[0].\
+                    set('modifiers', '{"tree_invisible": %s}' %
+                        (str(not '1' in ans_dict).lower())
+                        )
+
+            if 'a2_id' in line_fields:
+                line_fields['a2_id']['string'] = ans_dict.get('2', 'A2')
+                doc.xpath("//field[@name='a2_id']")[0].\
+                    set('modifiers', '{"tree_invisible": %s}' %
+                        (str(not '2' in ans_dict).lower())
+                        )
+
+            if 'a3_id' in line_fields:
+                line_fields['a3_id']['string'] = ans_dict.get('3', 'A3')
+                doc.xpath("//field[@name='a3_id']")[0].\
+                    set('modifiers', '{"tree_invisible": %s}' %
+                        (str(not '3' in ans_dict).lower())
+                        )
+
+            if 'a4_id' in line_fields:
+                line_fields['a4_id']['string'] = ans_dict.get('4', 'A4')
+                doc.xpath("//field[@name='a4_id']")[0].\
+                    set('modifiers', '{"tree_invisible": %s}' %
+                        (str(not '4' in ans_dict).lower())
+                        )
+
+            if 'a5_id' in line_fields:
+                line_fields['a5_id']['string'] = ans_dict.get('5', 'A5')
+                doc.xpath("//field[@name='a5_id']")[0].\
+                    set('modifiers', '{"tree_invisible": %s}' %
+                        (str(not '5' in ans_dict).lower())
+                        )
+
+            res['fields']['invoice_line'][
+                'views']['tree']['arch'] = etree.tostring(doc)
+        return res
+
+    # We override the Hook method of account_invoice
+    # to add our analytic codes
+    def finalize_invoice_move_lines(self, cr, uid, invoice_browse, move_lines):
+        print "************************************"
+        print invoice_browse
+        print "************************************"
+        print move_lines
+        print "************************************"
+        return super(
+            account_invoice_line_analytic,
+            self).finalize_invoice_move_lines(
+                cr, uid, invoices_browse, move_lines)
