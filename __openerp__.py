644d10f826fde0cdfafdf2835c2ed8d6da9dc1aa
Alexandre Brun <alexandre.brun@xcg-consulting.fr>
2014-12-16 11:36:27 +0100

    Bumped to version 1.6.4 (dev)

--- __openerp__.py
+++ __openerp__.py
@@ -4,7 +4,7 @@
 ##############################################################################
 {
     "name": "Account Invoice Streamline",
-    "version": "1.6.3",
+    "version": "1.6.4",
     "author": "XCG Consulting",
     "category": 'Accounting',
     "description": """Enhancements to the account
